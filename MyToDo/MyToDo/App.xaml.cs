ac814717567fc7cb88391e62c8202af85801e0f2
苗金波 <>
2023-04-24 15:31:19 +0800

    首页-导航条

--- MyToDo/MyToDo/App.xaml.cs
+++ MyToDo/MyToDo/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using Prism.DryIoc;
 using Prism.Ioc;
 
@@ -15,11 +9,19 @@ namespace MyToDo
     /// </summary>
     public partial class App : PrismApplication
     {
+        /// <summary>
+        /// 启动页
+        /// </summary>
+        /// <returns></returns>
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
         }
 
+        /// <summary>
+        /// 依赖注入
+        /// </summary>
+        /// <param name="containerRegistry"></param>
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             
--- MyToDo/MyToDo/MainWindow.xaml.cs
+++ MyToDo/MyToDo/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace MyToDo
 {
@@ -23,6 +15,51 @@ namespace MyToDo
         public MainWindow()
         {
             InitializeComponent();
+
+            minBtn.Click += (sender, args) =>
+            {
+                this.WindowState = WindowState.Minimized;
+            };
+
+            maxBtn.Click += (sender, args) =>
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                }
+            };
+
+            closeBtn.Click += (sender, args) =>
+            {
+                this.Close();
+            };
+
+            //colorZone鼠标拖动事件
+            colorZone.MouseMove += (sender, args) =>
+            {
+                //判断鼠标状态
+                if (args.LeftButton == MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            };
+            
+            //colorZone鼠标双击事件
+            colorZone.MouseDoubleClick += (sender, args) =>
+            {
+                if (this.WindowState == WindowState.Normal)
+                {
+                    this.WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+            };
         }
     }
 }
