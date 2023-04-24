fb48b73b3680ff9c348c07aade76ca0dfe22f0de
苗金波 <>
2023-04-24 12:53:00 +0800

    add package

--- MyToDo/MyToDo/App.xaml.cs
+++ MyToDo/MyToDo/App.xaml.cs
@@ -5,13 +5,24 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
 
 namespace MyToDo
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
     }
 }
